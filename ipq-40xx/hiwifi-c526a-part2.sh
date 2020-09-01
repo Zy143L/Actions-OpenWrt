129009f78926012ee512e82584a2226d732aa4d1
Zy143L <Zy143L@Dmkuai.cn>
2020-09-01 20:20:39 +0800

    Update hiwifi-c526a-part2.sh
    修正软件包

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -35,6 +35,7 @@ cat >> .config <<EOF
 # CONFIG_PACKAGE_shadowsocksr-libev-server is not set
 # CONFIG_PACKAGE_ddns-scripts_aliyun is not set
 # CONFIG_PACKAGE_ddns-scripts_dnspod is not set
+# CONFIG_PACKAGE_luci-app-nlbwmon is not set
 EOF
 
 # 其它默认的包 luci luci-app-upnp luci-app-nlbwmon luci-app-samba autosamba automount default-settings
@@ -46,7 +47,6 @@ CONFIG_PACKAGE_nano=y
 CONFIG_PACKAGE_wget=y
 CONFIG_PACKAGE_curl=y
 CONFIG_PACKAGE_unzip=y
-CONFIG_PACKAGE_screen=y
 CONFIG_PACKAGE_luci-app-turboacc=y
 CONFIG_PACKAGE_autocore-arm=y
 CONFIG_PACKAGE_luci-app-zerotier=y
@@ -55,23 +55,8 @@ CONFIG_PACKAGE_luci-app-turboacc=y
 CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_flow-offload=y
 CONFIG_PACKAGE_luci-app-turboacc_INCLUDE_bbr-cca=y
 CONFIG_PACKAGE_luci-app-ssr-python-pro-server=y
-CONFIG_PACKAGE_luci-app-openvpn-server=y
-CONFIG_PACKAGE_luci-app-dump1090=y
-EOF
-
-# FFmpeg
-#cat >> .config <<EOF
-#CONFIG_PACKAGE_libopus=y
-#CONFIG_PACKAGE_libx264=y
-#CONFIG_PACKAGE_ffmpeg=y
-#CONFIG_PACKAGE_ffprobe=y
-#EOF
-
-# 常用软件 默认已启用
-cat >> .config <<EOF
 CONFIG_PACKAGE_luci=y
 CONFIG_PACKAGE_luci-app-upnp=y
-# CONFIG_PACKAGE_luci-app-nlbwmon is not set
 CONFIG_PACKAGE_luci-app-samba=y
 CONFIG_PACKAGE_autosamba=y
 CONFIG_PACKAGE_default-settings=y
